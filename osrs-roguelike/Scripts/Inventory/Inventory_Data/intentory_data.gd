2ce17eeba545ec3b2e91432101c1d52f8bfd4bd2
mwalgren <michaelawalgren@gmail.com>
2026-08-06 11:48:16 -0700

    Inventory Update P2
    Stackable, draggable items that can be split into single quantities.

--- osrs-roguelike/Scripts/Inventory/Inventory_Data/intentory_data.gd
+++ osrs-roguelike/Scripts/Inventory/Inventory_Data/intentory_data.gd
@@ -1,4 +1,50 @@
 extends Resource
 class_name InventoryData
 
+signal inventory_interact(inventory_data:InventoryData, index:int, button:int)
+signal inventory_update(inventory_data:InventoryData)
+
 @export var slot_data_array:Array [SlotData]
+
+
+func grab_slot_data(index:int) -> SlotData:
+	var slot_data = slot_data_array[index]
+	
+	if slot_data:
+		slot_data_array[index] = null
+		inventory_update.emit(self)
+		return slot_data
+	else:
+		return null
+
+
+func on_slot_clicked(index:int, button:int) ->void:
+	inventory_interact.emit(self, index, button)
+
+func drop_slot_data(slot_drag_data:SlotData, index:int) -> SlotData:
+	var slot_data = slot_data_array[index]
+	
+	var return_slot_data:SlotData
+	
+	if slot_data and slot_data.can_merge_slot(slot_drag_data):
+		slot_data.merge_slots(slot_drag_data)
+	else: 
+		slot_data_array[index] = slot_drag_data
+		return_slot_data = slot_data
+
+	inventory_update.emit(self)
+	return return_slot_data
+
+func drop_single_slot_data(slot_drag_data:SlotData, index:int) ->SlotData:
+	var slot_data = slot_data_array[index]
+	
+	if not slot_data:
+		slot_data_array[index] = slot_drag_data.create_single_slot_data()
+	elif slot_data.can_merge_one(slot_drag_data):
+		slot_data.merge_slots(slot_drag_data.create_single_slot_data())
+		
+	inventory_update.emit(self)
+		
+	if slot_drag_data.quantity > 0:
+		return slot_drag_data
+	else: return null
--- osrs-roguelike/Scripts/Inventory/Inventory_Data/inventory_interface.gd
+++ osrs-roguelike/Scripts/Inventory/Inventory_Data/inventory_interface.gd
@@ -1,6 +1,37 @@
 extends Control
 
-@export var test_player_inv:Resource
+@export var test_player_inv_panel:PanelContainer
+@export var test_inv:Resource #TESTING ONLY
 
-func set_player_inventory_data(inv_data:InventoryData) ->void:
-	pass
+var slot_drag_data:SlotData
+@onready var inventory_slot: PanelContainer = $InventorySlot
+
+func _physics_process(delta: float) -> void:
+	if inventory_slot.visible:
+		inventory_slot.global_position = get_global_mouse_position() + Vector2(5,5)
+
+func _ready() -> void:
+	set_player_inventory_data(test_inv)
+
+func set_player_inventory_data(inv_data:InventoryData) -> void:
+	test_player_inv_panel.set_inventory_data(inv_data)
+	inv_data.inventory_interact.connect(on_inventory_interact)
+	
+func on_inventory_interact(inventory_data:InventoryData, index:int, button:int):
+		match [slot_drag_data, button]:
+			[null, MOUSE_BUTTON_LEFT]:
+				slot_drag_data = inventory_data.grab_slot_data(index)
+			[_, MOUSE_BUTTON_LEFT]:
+				slot_drag_data = inventory_data.drop_slot_data(slot_drag_data, index)
+			[null, MOUSE_BUTTON_RIGHT]:
+				pass
+			[_, MOUSE_BUTTON_RIGHT]:
+				slot_drag_data = inventory_data.drop_single_slot_data(slot_drag_data, index)
+		update_dragged_slot()
+
+func update_dragged_slot() -> void:
+	if slot_drag_data:
+		inventory_slot.show()
+		inventory_slot.set_slot_data(slot_drag_data)
+	else:
+		inventory_slot.hide()
