5b9d718dfb39515b7c4d7a268acc16063dbbfd49
mwalgren <michaelawalgren@gmail.com>
2026-08-10 18:27:47 -0700

    default deck being created

---
+++ osrs-roguelike/Scripts/Player/new_player_creation.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name Character
+
+
+var player_inventory:Array [InventoryData] = []
+
+
+
+@export_category("Skills")
+@export var attack_level:int = 1 
+@export var strength_level:int = 1 
+@export var magic_level:int = 1 
+@export var prayer_level:int = 1 
+@export var ranged_level:int = 1 
+
+
+@export var character_name:String
+
+@export var base_deck:Dictionary
